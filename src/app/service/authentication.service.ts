b72f0de730310862c617da2e69befda26a3abdff
dell <dell@DESKTOP-4P8U8AR>
2024-08-11 19:10:26 +0100

    Authentication: Register service call

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {environment} from '../../environments/environment';
 import {HttpClient, HttpErrorResponse, HttpResponse} from '@angular/common/http';
 import {Observable} from "rxjs";
+import {User} from "../model/user";
 
 @Injectable({providedIn: 'root'})
 export class AuthenticationService {
@@ -12,5 +13,9 @@ export class AuthenticationService {
 
   public login(user: User) : Observable<HttpResponse<any> | HttpErrorResponse> {
     return this.http.post<HttpResponse<any> | HttpErrorResponse>(`${this.host}/user/login`, user,{observe:'response'});
+  }
+
+  public register(user: User) : Observable<User | HttpErrorResponse> {
+    return this.http.post<User | HttpErrorResponse>(`${this.host}/user/register`, user);
   }
 }
