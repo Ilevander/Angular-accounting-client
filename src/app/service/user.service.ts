b9bff1170a6f23236558b2c3c696073a5ba93cf6
dell <dell@DESKTOP-4P8U8AR>
2024-08-12 15:43:42 +0100

    User Service: resetPassword Service call

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -24,4 +24,7 @@ export class UserService {
     return this.http.post<User>(`${this.host}/user/update`, formData);
   }
 
+  public resetPassword(email: string): Observable<any | HttpErrorResponse> {
+    return this.http.get<User>(`${this.host}/user/resetpassword/${email}`);
+  }
 }
