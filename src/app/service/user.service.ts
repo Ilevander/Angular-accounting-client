872a6a6af6f41c77dcd9f8f3840e8105676e9a2a
dell <dell@DESKTOP-4P8U8AR>
2024-08-12 15:26:15 +0100

    User Service: User list service call

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient, HttpErrorResponse, HttpResponse} from '@angular/common/http';
+import {Observable} from "rxjs";
+import {User} from "../model/user";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private host = environment.apiUrl;
+  constructor(private http: HttpClient) { }
+
+  public getUsers(): Observable<User[] | HttpErrorResponse> {
+      return this.http.get<User[]>(`${this.host}/user/list`);
+  }
 }
