96403c51ea2fb14539e2daba1cdeae9cae92d47d
dell <dell@DESKTOP-4P8U8AR>
2024-08-13 02:34:18 +0100

    Interceptor: Configure Http_interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {HttpClientModule} from '@angular/common/http';
-
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {BrowserModule} from "@angular/platform-browser";
+import {AuthenticationService} from "./service/authentication.service";
+import {UserService} from "./service/user.service";
+import {AuthInterceptor} from "./interceptor/auth.interceptor";
 
 
 @NgModule({
@@ -9,6 +12,13 @@ import {HttpClientModule} from '@angular/common/http';
   imports: [
     CommonModule,
     HttpClientModule,
-  ]
+    BrowserModule,
+  ],
+  providers: [
+    AuthenticationService,
+    UserService,
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor , multi: true},//multi instence of injection into the interceptor
+  ],
 })
+
 export class AppModule { }
